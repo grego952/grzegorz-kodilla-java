9d7dc40a1551c984e39ab068b6e962334a9fdaa8
Grzegorz <karaluch26@wp.pl>
2020-11-20 20:39:23 +0100

    Task 7.1: Text Beautifier

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,25 +1,30 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.ExecuteSaySomething;
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.lambda.Processor;
-import com.kodilla.stream.lambda.SaySomething;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {                                   // [4]
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();       // [5]
 
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        PoemDecorator beautifulString = (text) -> "Raz, dwa, trzy " + text;
+        PoemDecorator metal = (text) -> "<<<" + text +">>>";
 
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        String text1 = poemBeautifier.beautify("Koczkodan",String::toUpperCase);
+        String text2 = poemBeautifier.beautify("Karmazyn", String::toLowerCase);
+        String text3 = poemBeautifier.beautify("Baba Jaga patrzy", beautifulString);
+        String text4 = poemBeautifier.beautify("Metallica", metal);
+
+
+        System.out.println(text1);
+        System.out.println(text2);
+        System.out.println(text3);
+        System.out.println(text4);
+
+
+
     }
 }
