fb8cd5e8b9dfe1ac3790fc7478cf5082eab188b9
Grzegorz <karaluch26@wp.pl>
2020-11-21 22:48:03 +0100

    corrected age calculation

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,27 +1,31 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.book.Book;
-import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
 
+import java.time.Year;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
+
+
+
         Forum forum = new Forum();
         Map <Integer, ForumUser> forumUsers = forum.getUsers().stream()
+
                 .filter(forumUser -> forumUser.getSex() == 'M')
-                .filter(forumUser -> forumUser.getDateOFBirth().getYear() < 2000)
+                .filter(forumUser -> forumUser.getAge() > 20)
                 .filter(forumUser -> forumUser.getNumberOfPosts() >= 1)
                 .collect(Collectors.toMap(ForumUser::getUserID, forumUser -> forumUser));
 
                 forumUsers.entrySet().stream()
                 .map(entry -> entry.getKey() + " " + entry.getValue())
                 .forEach(System.out::println);
+
 
     }
 }
