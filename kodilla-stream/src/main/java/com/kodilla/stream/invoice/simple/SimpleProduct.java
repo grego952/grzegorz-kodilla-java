a6abfcf414db3b53368ee16abe8efe78dbdaaa15
Grzegorz <karaluch26@wp.pl>
2020-11-21 23:22:49 +0100

    Task 7.4

--- kodilla-stream/src/main/java/com/kodilla/stream/invoice/simple/SimpleProduct.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/invoice/simple/SimpleProduct.java
@@ -1,2 +1,28 @@
-package com.kodilla.stream.invoice.simple;public class SimpleProduct {
+package com.kodilla.stream.invoice.simple;
+
+public class SimpleProduct {
+    private final String productName;
+    private final double ProductPrice;
+
+    public SimpleProduct(final String productName, final double productPrice) {
+        this.productName = productName;
+        ProductPrice = productPrice;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getProductPrice() {
+        return ProductPrice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SimpleProduct)) return false;
+        SimpleProduct that = (SimpleProduct) o;
+        return productName.equals(that.productName);
+    }
+
 }
