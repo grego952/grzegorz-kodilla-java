e8134e6df08d44e4f639dfd700c0400813d604e1
Grzegorz <karaluch26@wp.pl>
2021-01-14 19:18:56 +0100

    14.2 Beans Configuration

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -0,0 +1,33 @@
+package com.kodilla.spring.library;
+
+import com.kodilla.spring.Library.Library;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+public class LibraryTestSuite {
+
+    @Autowired
+    private Library library;
+
+    @Test
+    void testLoadFromDb() {
+        //Given
+        //When
+        library.loadFromDb();
+        //Then
+        //Do nothing
+    }
+
+    @Test
+    void testSaveToDb() {
+        //Given
+        //When
+        library.saveToDb();
+        //Then
+        //Do nothing
+    }
+}
