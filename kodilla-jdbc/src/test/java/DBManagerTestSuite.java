cb86654f07570c624dacd3545bb7e62faa299769
Grzegorz <karaluch26@wp.pl>
2021-01-29 17:58:45 +0100

    added test for selecting users and posts

--- kodilla-jdbc/src/test/java/DBManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/DBManagerTestSuite.java
@@ -43,4 +43,32 @@ public class DBManagerTestSuite {
         statement.close();
         assertEquals(5, counter);
     }
+
+    @Test
+    void testSelectUsersAndPosts() throws SQLException {
+
+        //Given
+        DBManager dbManager = DBManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT U.FIRSTNAME, U.LASTNAME\n" +
+                "FROM USERS U\n" +
+                "JOIN POSTS P ON U.ID = P.USER_ID\n" +
+                "GROUP BY USER_ID\n" +
+                "HAVING COUNT(*) >= 2";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        assertEquals(1, counter);
+    }
+
 }
