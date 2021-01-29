9842ae64d124dd45e5d587690283b3de64c6eb9a
Grzegorz <karaluch26@wp.pl>
2021-01-29 17:51:09 +0100

    module 16.6 connecting to database

---
+++ kodilla-jdbc/src/test/java/DBManagerTestSuite.java
@@ -0,0 +1,47 @@
+import com.kodilla.jdbc.DBManager;
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class DBManagerTestSuite {
+
+    @Test
+    void testConnection() throws SQLException {
+
+        //Given
+        //When
+        DBManager dbManager = DBManager.getInstance();
+        //Then
+        assertNotNull(dbManager.getConnection());
+    }
+
+    @Test
+    void testSelectUsers() throws SQLException {
+
+        //Given
+        DBManager dbManager  = DBManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getInt("ID") + ", " +
+                    rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        assertEquals(5, counter);
+
+    }
+}
