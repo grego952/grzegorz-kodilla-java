2fca8a1f0a865641bab0d021d9de6fe47028fb40
Grzegorz <karaluch26@wp.pl>
2021-02-04 12:18:08 +0100

    entities relations

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -0,0 +1,64 @@
+package com.kodilla.hibernate.manytomany.dao;
+
+import com.kodilla.hibernate.manytomany.Company;
+import com.kodilla.hibernate.manytomany.Employee;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@SpringBootTest
+public class CompanyDaoTestSuite {
+
+    @Autowired
+    private CompanyDao companyDao;
+
+    @Test
+    void testSaveManyToMany() {
+
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaestres = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMaestres.getEmployees().add(stephanieClarckson);
+        dataMaestres.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaestres);
+        lindaKovalsky.getCompanies().add(dataMaestres);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        //When
+        companyDao.save(softwareMachine);
+        int softwareMachineId = softwareMachine.getId();
+        companyDao.save(dataMaestres);
+        int dataMaestersId = dataMaestres.getId();
+        companyDao.save(greyMatter);
+        int greyMatterId = greyMatter.getId();
+
+        //Then
+        assertNotEquals(0, softwareMachineId);
+        assertNotEquals(0, dataMaestersId);
+        assertNotEquals(0, greyMatterId);
+
+        //CleanUp
+//        try {
+//            companyDao.deleteById(softwareMachineId);
+//            companyDao.deleteById(dataMaestersId);
+//            companyDao.deleteById(greyMatterId);
+//        } catch (Exception e) {
+//            //do nothing
+//        }
+
+    }
+}
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -1,24 +1,29 @@
 package com.kodilla.hibernate.tasklist.dao;
 
+import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.TaskFinancialDetails;
 import com.kodilla.hibernate.tasklist.TaskList;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 public class TaskListDaoTestSuite {
 
     @Autowired
     private TaskListDao taskListDao;
+    private static final String LISTNAME = "House Cleaning";
 
     @Test
     void testFindByListName() {
         //Given
-        TaskList taskList = new TaskList("House Cleaning", "List describing activities to keep your house clean");
+        TaskList taskList = new TaskList(LISTNAME, "List describing activities to keep your house clean");
         taskListDao.save(taskList);
         String taskName = taskList.getListName();
 
@@ -32,4 +37,31 @@ public class TaskListDaoTestSuite {
         int id = readList.get(0).getId();
         taskListDao.deleteById(id);
     }
+
+    @Test
+    void testTaskListDaoSaveWithTasks() {
+        //Given
+        Task task = new Task("Test: Learn Hibernate", 14);
+        Task task2 = new Task("Write some entities", 3);
+        TaskFinancialDetails tfd = new TaskFinancialDetails(new BigDecimal(120), false);
+        TaskFinancialDetails tfd2 = new TaskFinancialDetails(new BigDecimal(10), false);
+        task.setTaskFinancialDetails(tfd);
+        task2.setTaskFinancialDetails(tfd2);
+        TaskList taskList = new TaskList(LISTNAME, "ToDo tasks");
+        taskList.getTasks().add(task);
+        taskList.getTasks().add(task2);
+        task.setTaskList(taskList);
+        task2.setTaskList(taskList);
+
+        //When
+        taskListDao.save(taskList);
+        int id = taskList.getId();
+
+        //Then
+        assertNotEquals(0, id);
+
+        //CleanUp
+        taskListDao.deleteById(id);
+    }
+
 }
